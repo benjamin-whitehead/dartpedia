7a3b76dd1069c787573de838f6f00c40d80cee8c
Benjamin Whitehead <benwhitehead21@gmail.com>
2019-05-11 14:07:43 -0400

    Deleted files

--- ios/Classes/DartpediaPlugin.m
+++
@@ -1,8 +0,0 @@
-#import "DartpediaPlugin.h"
-#import <dartpedia/dartpedia-Swift.h>
-
-@implementation DartpediaPlugin
-+ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
-  [SwiftDartpediaPlugin registerWithRegistrar:registrar];
-}
-@end

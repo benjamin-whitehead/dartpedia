8c93a4a4de4e8c8603b6ee2affe5d7e39645960d
Benjamin Whitehead <benwhitehead21@gmail.com>
2019-05-17 16:47:35 -0400

    finished travis cli integration

--- lib/src/wikipedia_response.dart
+++ lib/src/wikipedia_response.dart
@@ -1,7 +1,7 @@
-/// All possible Wikipedia Calls need to have it's own response, 
+/// All possible Wikipedia Calls need to have it's own response,
 /// this is to abstract the code back a layer to simplify large files
-/// Each Response needs to have a parse function, that is responsible 
-/// for gettings its data, and a clean function, that is responsible 
+/// Each Response needs to have a parse function, that is responsible
+/// for gettings its data, and a clean function, that is responsible
 /// for cleaning the data for use in the public API
 abstract class WikipediaResponse {
   Future parse(String topic, Map<String, dynamic> params);
